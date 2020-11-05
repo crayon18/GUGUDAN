9a5fcc1539c293888ad891acccb6806bf2615b35
PC <PC@121.173.188.193>
2020-11-06 00:30:09 +0900

    1111

--- src/GuguDanFourFive.java
+++ src/GuguDanFourFive.java
@@ -3,13 +3,12 @@ public class GuguDanFourFive {
 
 	public static void main(String[] args) {
 		int i = 1;
-		while(i < 10) {
-			System.out.println(4*i);
-			i += 1;
-		}
-		
+		//while(i < 10) {
+		//	System.out.println(4*i);
+		//	i += 1;
+	//	}
 		for(int j = 1; j<10; j++) {
-			System.out.println(7*j);
+			System.out.println(8*j);
 		}
 	}
 
